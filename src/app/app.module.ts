9259591c830e65c225368bed8c1ad884fc75a343
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-12 10:12:18 -0700

    add event class and event search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { routing } from  './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { MeetupSearchComponent } from './meetup-search/meetup-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    MeetupSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/event.model.ts
@@ -0,0 +1,3 @@
+export class Event {
+  constructor(public title: string, public topic: string, public date: string, public time: string, public location: string, public mentor: string, public description: string, public maximum: number, public guests: number, public image:string){}
+}
