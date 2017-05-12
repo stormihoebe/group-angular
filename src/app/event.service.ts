6c6b88b2bdfefea271d2b1c683e9c83da73d7b4c
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-12 10:49:13 -0700

    add function to search component to get all meetups to display

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import {Event} from './event.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class EventService {
+  events: FirebaseListObservable<any[]>;
 
-  constructor() {
+  constructor(private database: AngularFireDatabase) {
+    this.events= database.list('events');
  }
 
  getEvents(){
    console.log("HEY getEvents Here!");
+   return this.events;
  }
  getEventById(eventId: string){
+  return this.database.object('events/' + eventId);
 }
 
 }
--- src/app/meetup-search/meetup-search.component.ts
+++ src/app/meetup-search/meetup-search.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Event } from '../event.model';
+import { EventService } from '../event.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-meetup-search',
   templateUrl: './meetup-search.component.html',
-  styleUrls: ['./meetup-search.component.css']
+  styleUrls: ['./meetup-search.component.css'],
+  providers: []
 })
 export class MeetupSearchComponent implements OnInit {
-
-  constructor() { }
+  events;
+  constructor(public eventService: EventService, public router: Router) { }
 
   ngOnInit() {
+    this.events= this.eventService.getEvents();
   }
 
 }
