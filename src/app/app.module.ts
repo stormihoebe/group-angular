ed513ba77f63848130b7490502832bf072cd104d
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-12 10:39:42 -0700

    configured app for angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MeetupSearchComponent } from './meetup-search/meetup-search.component';
-import {EventService} from './event.service'
+import {EventService} from './event.service';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +31,9 @@ import {EventService} from './event.service'
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [EventService],
   bootstrap: [AppComponent]
